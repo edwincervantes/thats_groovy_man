91843c04cb7a3edd55ddb9635493dbf193657132
Edwin Cervantes <edwincervantes@Edwins-MacBook-Pro.local>
2025-07-21 15:09:15 -0400

    Doing HW in groovy

---
+++ src/com/test/demo/MyFirstGroovyProgram.groovy
@@ -0,0 +1,12 @@
+package com.test.demo
+
+class MyFirstGroovyProgram {
+
+	static void main(args) {
+		// Single line comment
+		/*
+		 * This is a multi-line comment
+		 */
+		println("Hello World!");
+	}
+}
